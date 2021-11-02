968a3b2416934b6240b6a519f3717aa6f38dc422
vfiftyfive <viftyfive@gmail.com>
2021-11-02 00:08:24 +0000

    remove key

--- marvel_init_db/build_and_run.sh
+++ marvel_init_db/build_and_run.sh
@@ -2,4 +2,4 @@
 
 docker rmi vfiftyfive/marvel_init_db -f
 docker buildx build --platform linux/amd64,linux/arm64 --push -t vfiftyfive/marvel_init_db .
-docker run --env API_PRIVATE_KEY="2ff768c5567a1e2035dcef735e2be04e7af86daa" --env MONGO_HOST="172.17.0.2" -d vfiftyfive/marvel_init_db
+docker run --env MONGO_HOST="172.17.0.2" -d vfiftyfive/marvel_init_db
